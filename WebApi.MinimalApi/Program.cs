40cc37d14064933b4354be7868d97b4698b1aeef
Jakob Ornysh <jornysh@gmail.com>
2025-09-30 15:46:39 +0500

    Task 1: GET /users/{userId}

--- WebApi.MinimalApi/Program.cs
+++ WebApi.MinimalApi/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+using WebApi.MinimalApi.Domain;
+using WebApi.MinimalApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5000");
-builder.Services.AddControllers()
+
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.CreateMap<UserEntity, UserDto>()
+        .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.LastName} {src.FirstName}"));
+}, new System.Reflection.Assembly[0]);
+
+builder.Services.AddControllers(options =>
+{
+    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+    options.ReturnHttpNotAcceptable = true;
+    options.RespectBrowserAcceptHeader = true;
+})
     .ConfigureApiBehaviorOptions(options => {
         options.SuppressModelStateInvalidFilter = true;
         options.SuppressMapClientErrors = true;
